40818da137be51dd9d62b2076cb83cd03031fdf4
Ram Rasekar <ram.rasekar946@gmail.com>
2017-07-14 21:02:19 +0530

    Allow Standalone Compilation

--- sound/soc/intel/effects.c
+++ sound/soc/intel/effects.c
@@ -224,7 +224,7 @@ static int sst_get_pipe_id(struct sst_dev_stream_map *map, int map_size,
 static int sst_effects_create(struct snd_card *card, struct snd_effect *effect)
 {
 	int ret = 0;
-	u8 pipe_id;
+	u8 pipe_id=pipe_id;
 	struct ipc_effect_payload dsp_payload;
 	struct ipc_dsp_effects_info effects_info;
 	struct sst_data *sst;
@@ -257,7 +257,7 @@ static int sst_effects_create(struct snd_card *card, struct snd_effect *effect)
 static int sst_effects_destroy(struct snd_card *card, struct snd_effect *effect)
 {
 	int ret = 0;
-	u8 pipe_id;
+	u8 pipe_id=pipe_id;
 	struct ipc_effect_payload dsp_payload;
 	struct ipc_dsp_effects_info effects_info;
 	struct sst_data *sst;
@@ -291,7 +291,7 @@ static int sst_effects_set_params(struct snd_card *card,
 					struct snd_effect_params *params)
 {
 	int ret = 0;
-	u8 pipe_id;
+	u8 pipe_id=pipe_id;
 	u16 algo_id;
 	struct ipc_effect_payload dsp_payload;
 	struct sst_data *sst;
@@ -325,7 +325,7 @@ static int sst_effects_get_params(struct snd_card *card,
 					struct snd_effect_params *params)
 {
 	int ret = 0;
-	u8 pipe_id;
+	u8 pipe_id=pipe_id;
 	u16 algo_id;
 	struct ipc_effect_payload dsp_payload;
 	struct sst_data *sst;
